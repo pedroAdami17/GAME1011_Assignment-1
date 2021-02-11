90ca5b69dfa36d8bb9a3d3637b73255e064dc0dc
Pedro Adami <pedroadami17@gmail.com>
2021-02-11 12:22:28 -0300

    Declared al classes and made the user interface.

--- GAME1011_Assignment 1/GAME1011_Assignment 1.cpp
+++ GAME1011_Assignment 1/GAME1011_Assignment 1.cpp
@@ -2,13 +2,47 @@
 //
 
 #include <iostream>
+using namespace std;
+
+int numOfStudents;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "Hello! This is a survey simulator program. \n";
+	cout << "Please enter the number of students that will participate between 1 and 500! \n";
+	cin >> numOfStudents;
+	cout << /*Here output the survey class ressults*/ endl;
 }
 
 class Person
 {
-	;;
+public:
+	string name;
+	int age;
 };
+
+class Student : public Person
+{
+public:
+	string collegeName;
+	string courseEnrolled;
+	int semester;
+};
+
+class NonGamingStudent : public Student
+{
+	string streamingServ;
+	int hoursNum;
+};
+
+class GamingStudent : public Student
+{
+	string gamingDevice;
+	int hoursNum;
+};
+
+class Survey
+{
+	
+};
+
